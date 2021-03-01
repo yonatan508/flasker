cb1aad2d640d53b297d60c757cac769cb741e5fe
yonatan Kazaz <yonatankazaz@gmail.com>
2021-03-01 23:18:03 +0200

    tweaked 500.html

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
 
 
 # create a flask app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "My game is not good"
+
+#create a form class
+class NamerForm(FlaskForm):
+    user_name = StringField("Enter User Name", validators=[DataRequired()])
+    user_password = PasswordField("Enter Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # create a route decorator
 @app.route("/")
@@ -32,3 +43,21 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route("/name", methods=['GET','POST'])
+def name():
+    name = None
+    password = None
+    form_user = NamerForm()
+    form_password = NamerForm()
+    #validate form
+    if form_user.validate_on_submit():
+        name = form_user.user_name.data
+        form_user.user_name.data = ''
+        password = form_password.user_password.data
+        form_password.user_password.data = ''
+    return render_template("name.html",
+        name = name,
+        password = password,
+        form_user = form_user,
+        form_password = form_password)
